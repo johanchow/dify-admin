9ba47021a9062b56844de2734a3cfe2ead1b8ce0
johan <johan>
2025-08-17 10:10:07 +0800

    配置上端口映射，可以本地调试nest服务

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -4,15 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
   }));
-  
+
   app.enableCors();
-  
-  const port = process.env.PORT || 3001;
+
+  const port = process.env.PORT || 3003;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
